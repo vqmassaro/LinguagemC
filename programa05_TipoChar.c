2ffaec949bf5c2f18d85efd40b77b4f5caa65d8a
vqmassaro <valmirmassaro@gmail.com>
2024-05-04 17:36:39 -0300

    o tipo char

---
+++ programa05_TipoChar.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    char letra0='C', letra1=' ', letra2='P', letra3='r', letra4='o', letra5='g', letra6='r', letra7='e', letra8='s', letra9='s', letra10='i', letra11='v', letra12='o';
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c", letra0, letra1, letra2, letra3, letra4, letra5, letra6, letra7, letra8, letra9, letra10, letra11, letra12);
+
+    return 0;
+}
